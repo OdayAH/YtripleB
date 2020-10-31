4b09de1be17f1dbe0c15c1d9e56e2186a993692d
Rasheedbh2 <63792722+Rasheedbh2@users.noreply.github.com>
2020-10-31 16:03:34 +0200

    jdeed jdeed bas men 3enna

---
+++ newnav.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css2/bootstrap.min.css">
+
+<!--  font awesome icons  -->
+<link rel="stylesheet" href="css2/all.min.css">
+
+
+<!--  Magnific Popup css file  -->
+<link rel="stylesheet" href="./PortFolio_Website-master/vendor/Magnific-Popup/dist/magnific-popup.css">
+
+
+<!--  Owl-carousel css file  -->
+<link rel="stylesheet" href="./PortFolio_Website-master/vendor/owl-carousel/css/owl.carousel.min.css">
+<link rel="stylesheet" href="./PortFolio_Website-master/vendor/owl-carousel/css/owl.theme.default.min.css">
+
+
+<!--  custom css file  -->
+
+
+<!--  Responsive css file  -->
+<link rel="stylesheet" href="css2/responsive.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="nav.css">
+</head>
+<body>
+<body class="hero-anime">	
+
+<div class="navigation-wrap bg-light start-header start-style">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <nav class="navbar navbar-expand-md navbar-light">
+                
+                    <a class="navbar-brand" href="https://front.codes/" target="_blank"></a>	
+                    
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav ml-auto py-4 py-md-0">
+                            <li class="nav-item pl-4 pl-md-0 ml-0 ml-md-4 active">
+                                <a class="nav-link "  href="index.php" >Home</a>
+                            </li>
+                            <li class="nav-item pl-4 pl-md-0 ml-0 ml-md-4">
+                                <a class="nav-link" href="about.php">About</a>
+                            </li>
+                         
+                            <li class="nav-item pl-4 pl-md-0 ml-0 ml-md-4">
+                                <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Services</a>
+                                <div class="dropdown-menu">
+                                    <a class="dropdown-item" href="seo.php">SEO</a>
+                                    <a class="dropdown-item" href="ppc.php">PPC</a>
+                                    <a class="dropdown-item" href="smm.php">SMM</a>
+                                    <a class="dropdown-item" href="email.php">Email Marketing</a>
+                                    <a class="dropdown-item" href="web.php">Web Design</a>
+                                </div>
+                            </li>
+                             <li class="nav-item pl-4 pl-md-0 ml-0 ml-md-4">
+                                <a class="nav-link" href="#">Blog</a>
+                            </li>
+                            <li class="nav-item pl-4 pl-md-0 ml-0 ml-md-4">
+                                <a class="nav-link" href="#">Contact</a>
+                            </li>
+                        </ul>
+                    </div>
+                    
+                </nav>		
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<!-- Link to page
+================================================== -->
+
+
+<script src="nav.js"></script>
+   <!--  Jquery js file  -->
+   <script src="./PortFolio_Website-master/js/jquery.3.4.1.js"></script>
+
+<!--  Bootstrap js file  -->
+<script src="./PortFolio_Website-master/js/bootstrap.min.js"></script>
+
+<!--  isotope js library  -->
+<script src="./PortFolio_Website-master/vendor/isotope/isotope.min.js"></script>
+
+<!--  Magnific popup script file  -->
+<script src="./PortFolio_Website-master/vendor/Magnific-Popup/dist/jquery.magnific-popup.min.js"></script>
+
+<!--  Owl-carousel js file  -->
+<script src="./PortFolio_Website-master/vendor/owl-carousel/js/owl.carousel.min.js"></script>
+</body>
+</body>
+</html>
