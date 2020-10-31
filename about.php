9be3804d1c25f8f3593ad5c844fe380b5b245fd3
HolyCreateMedia <holycreatemedia@gmail.com>
2020-10-31 13:12:40 +0200

    about us

--- about.php
+++ about.php
@@ -1,19 +1,74 @@
-<!DOCTYPE html>
 <html lang="en">
 <head>
+    <style>
+        .bg{
+            background-color:#EBECF0;
+            margin-left:0;
+            padding-left:0;
+            margin-right:0;
+            padding-right:0;
+        }
+
+        #banner{
+background-image: url(assets/aboutus.jpg);
+background-repeat: no-repeat;
+background-size: cover ;
+width: 100%;
+height: 110vh;
+margin-top: -98px;
+z-index: -1;
+
+    }    
+    </style>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<head>
     <title>About Us</title>
 </head>
 <body>
-    <?php 
-        include('navbar.php');
-    ?>
+<?php
+ include('navbar.php');
+ ?>
+ <div id="banner">
 
+ </div>
+ <div class=" gtco-feature" id="section1">
+    <div class="container">
+        <div class="row">
+        <div class="row">
+        <div class="col-md-9">
+        <h1>Y-TripleB is a fully integrated branding and marketing agency.</h1>
+        <p>Powered by leading creativity, strategy building and teamwork. Our services are tailored to every client’s specific needs. With a full suite of capabilities</p>
+        </div>
+        </div>
+            <div class="row">
+                <div class="col-md-9">
+                    <h1>ABOUT Y-TRIPLEB MARKETING</h1>
+                    <p>Y-TripleB Marketing is a Lebanon-based, full-service digital marketing company. Like you, we are entrepreneurs focused on providing exceptional service to our clients. What makes us different? Our tools, our technology, and most importantly, our teams. That’s right teams. When you partner with Y-TripleB to grow your business online you get a dedicated team of industry experts who collaborate on everything from your digital marketing strategy to design and development, to analytics and optimization, to content and performance management. We take a deep dive into your business and growth goals, and use proven strategies to help make them happen.
+                    We’ve invested thousands of hours and dollars in developing the right technology, hiring the right people, and creating the right tools to help our clients understand their marketplaces and make the decisions that help them grow their businesses year-after-year. We can help YOU too!
 
-        <?php 
-        include('footer.php');
-    ?>
+                   </p>
+                </div>
+            </div>
+            <div class="container bg"> 
+            <div class="row mr-0 ml-0 pl-0 pr-0 ">
+                <div class="col-md-9 offset-md-2 ">
+                    <h1 class="offset-md-5 pt-3">Our Mission</h1>
+                    <p class="text-dark pb-4">We believe a cutting-edge company deserves recognition as an industry authority. We also believe that the biggest obstacle standing between innovative businesses and market domination is optimal visibility—the challenge of getting in front of the right buyers at the right time in the right way. Our purpose is to position exceptional businesses at the forefront of their industry using data-backed marketing strategies that evolve alongside an ever-advancing digital world.
+                    At Y-TripleB Marketing, we believe marketing is something much more powerful and more complex than clever ad campaigns. We define marketing as the process of establishing, cultivating, and nurturing long-term customer relationships. Companies who lead their industry do so because they have earned the trust and loyalty of their buyers—a feat that requires a customer-centered approach throughout all departments.
+                    This is precisely what our innovative framework sets out to do. We pair in-depth gap analysis, cutting edge campaigns, and tech-forward execution with extensive team training.
+                    Our data-driven marketing strategy begins with research and discovery designed to track the customer journey, predict and plan for evolving landscapes, conduct gap analysis, and determine essential KPIs for measuring success. In strategy execution, we incorporate advanced automation tools to implement our unique system of cross-platform advertisement and ongoing analysis. We subject each marketing scheme to quarterly review, consulting new data on campaign performance, audience values, and industry innovations to identify untapped opportunities or predict potential pitfalls. This allows us to make sure our client’s brand is always at the forefront of innovation and cultural evolution.
+                    Along the way, we provide team training, business advisory, and make our team available for on-demand meetings, recognizing that new obstacles and pressing questions do not abide by a schedule.
+                    Through this comprehensive framework, we are able to fulfill our mission of helping worthy businesses shape a meaningful reputation in all areas of customer interaction and secure their place as a market leader.    
+
+                    </p>
+                </div>
+        </div>
+        </div>
+        </div>
+        
+ <?php
+ include('footer.php');
+ ?>
+
 </body>
 </html>
